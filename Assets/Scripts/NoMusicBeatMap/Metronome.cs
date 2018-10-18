29e4e0099bcb16f48e604851f452659741cd6b28
Kyle Conway <kyleconway@rocketmail.com>
2018-10-18 14:09:18 -0400

    Basic Enemy AI
    -Simple NoMusic BeatMap (Just a time variable = .5 seconds)
    -Basic Enemy AI Structure (FSM Structure, swaps between move/punch states every .5 * Random(1-x) seconds)
    -Basic Metronome to keep beatmap in sync, goes off every .5 seconds. Oscillates from large to small.

---
+++ Assets/Scripts/NoMusicBeatMap/Metronome.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Dummy metronome used to keep beat (transforms approximately every .5seconds)
+/// </summary>
+/// 
+
+public class Metronome : Timer
+{
+
+    bool scaleControl = true;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("scaleMetronome", 0.0f, noMusicTimer);
+    }
+
+    void scaleMetronome()
+    {
+        if(scaleControl)
+        {
+            // Bigger
+            gameObject.transform.localScale += new Vector3(3, 3, 3);
+            scaleControl = false;
+        }
+        else if(!scaleControl)
+        {
+            // Smaller
+            gameObject.transform.localScale -= new Vector3(3, 3, 3);
+            scaleControl = true;
+        }
+    }
+}
